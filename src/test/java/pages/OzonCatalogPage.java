c72bd67a89f910dd0371be25e6187def6b3096fb
Alexey Kosovskiy <al.kosovsky@gmail.com>
2022-05-12 22:53:51 +0300

    Сортировка по цене

--- src/test/java/pages/OzonCatalogPage.java
+++ src/test/java/pages/OzonCatalogPage.java
@@ -4,11 +4,16 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OzonCatalogPage {
     private static final SelenideElement showAllBrandsButton = $x("//div[contains(text(), 'Бренды')]/..//span[contains(text(), 'Посмотреть все')]");
     private static final SelenideElement brandSearchInput = $x("//div[contains(text(), 'Бренды')]/..//input[@type='text']");
     private static final SelenideElement nothingFoundSpan = $x("//div[contains(text(), 'Бренды')]/..//span[text()='Ничего не нашлось']");
+    private static final SelenideElement sortingInput = $x("//input[@name='filter' and @role = 'combobox']");
+    private static final SelenideElement sortByPriceAsc = $x("//*[contains(text(),'Сначала дешёвые')]");
+    private static final SelenideElement firstProductPrice = $x("(//div[@data-widget = 'megaPaginator']//span[contains(text(), '₽') and contains(@style, 'ozAccentAlert')])[1]");
+    private static final SelenideElement secondProductPrice = $x("(//div[@data-widget = 'megaPaginator']//span[contains(text(), '₽') and contains(@style, 'ozAccentAlert')])[2]");
 
     public void selectBrand(String name) {
         showAllBrandsButton.click();
@@ -25,5 +30,17 @@ public class OzonCatalogPage {
 
     public void checkBrandIsNotFound() {
         nothingFoundSpan.shouldBe(Condition.visible);
+    }
+
+    public void sortByPriceAsc() {
+        sortingInput.click();
+        sortByPriceAsc.shouldBe(Condition.enabled).click();
+    }
+
+    public void checkProductsSortingByPriceAsc() {
+        $x("//div[contains(text(),'Сначала дешёвые')]").shouldBe(Condition.visible);
+        int firstPrice = Integer.parseInt(firstProductPrice.getText().substring(0, firstProductPrice.getText().indexOf("₽")-1));
+        int secondPrice = Integer.parseInt(secondProductPrice.getText().substring(0, secondProductPrice.getText().indexOf("₽")-1));
+        assertTrue(firstPrice <= secondPrice);
     }
 }
--- src/test/java/steps/StepDefs.java
+++ src/test/java/steps/StepDefs.java
@@ -9,26 +9,38 @@ import pages.OzonMainPage;
 public class StepDefs {
 
     @Когда("пользователь выбирает категорию товара {string}")
-    public void пользовательВыбираетКатегориюТовара(String name) {
+    public void openCatalogCategory(String name) {
         OzonMainPage page = new OzonMainPage();
         page.openCategory(name);
     }
 
     @И("выбирает бренд {string}")
-    public void выбираетБренд(String name) {
+    public void selectBrand(String name) {
         OzonCatalogPage page = new OzonCatalogPage();
         page.selectBrand(name);
     }
 
     @Тогда("отображаются товары выбранного бренда {string}")
-    public void отображаютсяТоварыВыбранногоБренда(String name) {
+    public void checkActiveFilterByBrand(String name) {
         OzonCatalogPage page = new OzonCatalogPage();
         page.checkActiveFilterByBrand(name);
     }
 
     @Тогда("отображается сообщение о том, что бренд не найден")
-    public void отображаетсяСообщениеОТомЧтоБрендНеНайден() {
+    public void checkBrandIsNotFound() {
         OzonCatalogPage page = new OzonCatalogPage();
         page.checkBrandIsNotFound();
+    }
+
+    @И("выбирает сортировку по цене по возрастанию")
+    public void sortByPriceAsc() {
+        OzonCatalogPage page = new OzonCatalogPage();
+        page.sortByPriceAsc();
+    }
+
+    @Тогда("товары отображаются по возрастанию цены")
+    public void checkProductsSortingByPriceAsc() {
+        OzonCatalogPage page = new OzonCatalogPage();
+        page.checkProductsSortingByPriceAsc();
     }
 }
